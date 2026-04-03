46ad40d94b1445b8c95c8301d55b29b65b1e681f
Atticus Foster <atticus.foster@maine.edu>
2026-04-02 21:01:41 -0400

    display now works, using built in pico library. will rewrite for complexity if time permits

--- Capstone_Code_Pico/src/capstone_main.c
+++ Capstone_Code_Pico/src/capstone_main.c
@@ -1,10 +1,105 @@
 #include <stdio.h>
 #include <string.h>
 #include "capstone_display.h"
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+#include "raspberry26x32.h"
+#include "ssd1306_font.h"
+#include <stdint.h>
 
-int main(void)
-{
-    start_i2c();
-    oled_init();
-    test();
+int main() {
+    stdio_init_all();
+
+    // I2C is "open drain", pull ups to keep signal high when no data is being
+    // sent
+    i2c_init(i2c_default, SSD1306_I2C_CLK * 1000);
+    gpio_set_function(0, GPIO_FUNC_I2C);
+    gpio_set_function(1, GPIO_FUNC_I2C);
+    gpio_pull_up(0);
+    gpio_pull_up(1);
+
+    // run through the complete initialization process
+    SSD1306_init();
+
+    // Initialize render area for entire frame (SSD1306_WIDTH pixels by SSD1306_NUM_PAGES pages)
+    struct render_area frame_area = {
+        start_col: 0,
+        end_col : SSD1306_WIDTH - 1,
+        start_page : 0,
+        end_page : SSD1306_NUM_PAGES - 1
+        };
+
+    calc_render_area_buflen(&frame_area);
+
+    // zero the entire display
+    uint8_t buf[SSD1306_BUF_LEN];
+    memset(buf, 0, SSD1306_BUF_LEN);
+    render(buf, &frame_area);
+
+    // render 3 cute little raspberries
+    struct render_area area = {
+        start_page : 0,
+        end_page : (IMG_HEIGHT / SSD1306_PAGE_HEIGHT)  - 1
+    };
+
+restart:
+
+    area.start_col = 0;
+    area.end_col = IMG_WIDTH - 1;
+
+    calc_render_area_buflen(&area);
+
+    uint8_t offset = 5 + IMG_WIDTH; // 5px padding
+
+    for (int i = 0; i < 3; i++) {
+        render(raspberry26x32, &area);
+        area.start_col += offset;
+        area.end_col += offset;
+    }
+
+    SSD1306_scroll(true);
+    sleep_ms(5000);
+    SSD1306_scroll(false);
+
+    char *text[] = {
+        "A long time ago",
+        "  on an ball ",
+        "   display",
+        " far far away",
+        "Lived a small",
+        "penis",
+        "by the name of",
+        "dick butt"
+    };
+
+    int y = 0;
+    for (uint i = 0 ;i < count_of(text); i++) {
+        WriteString(buf, 5, y, text[i]);
+        y+=8;
+    }
+    render(buf, &frame_area);
+
+    // Test the display invert function
+    sleep_ms(3000);
+    SSD1306_send_cmd(SSD1306_SET_INV_DISP);
+    sleep_ms(3000);
+    SSD1306_send_cmd(SSD1306_SET_NORM_DISP);
+
+    bool pix = true;
+    for (int i = 0; i < 2;i++) {
+        for (int x = 0;x < SSD1306_WIDTH;x++) {
+            DrawLine(buf, x, 0,  SSD1306_WIDTH - 1 - x, SSD1306_HEIGHT - 1, pix);
+            render(buf, &frame_area);
+        }
+
+        for (int y = SSD1306_HEIGHT-1; y >= 0 ;y--) {
+            DrawLine(buf, 0, y, SSD1306_WIDTH - 1, SSD1306_HEIGHT - 1 - y, pix);
+            render(buf, &frame_area);
+        }
+        pix = false;
+    }
+
+    goto restart;
+
+    return 0;
 }
